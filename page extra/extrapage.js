f8435da386f5afd81b01f71c28dd324cdaede465
JoeFikis <86575830+crazyplayzyt@users.noreply.github.com>
2022-04-14 17:06:25 -0500

    Add files via upload

--- page extra/extrapage.js
+++ page extra/extrapage.js
@@ -18,30 +18,7 @@ const highlightMenu = () => {
   const servicesMenu = document.querySelector('#services-page');
   let scrollPos = window.scrollY;
   // console.log(scrollPos);
-
-  // adds 'highlight' class to my menu items
-  if (window.innerWidth > 960 && scrollPos < 600) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 1400) {
-    aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 2345) {
-    servicesMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  }
-
-  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
-    elem.classList.remove('highlight');
-  }
 };
-
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
 
 //  Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
